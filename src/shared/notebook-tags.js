66279a5864ab544b947872dd9f68f9b7a1de8ab7
Mena Arguedas <maxx.alonso.mena.arguedas@intel.com>
2024-09-17 10:06:55 -0600

    changes done to notebook-tags including the formats expected to handle

--- src/shared/notebook-tags.js
+++ src/shared/notebook-tags.js
@@ -10,6 +10,45 @@ export const CATEGORIES = /** @type {const} */ ({
   XAI: 'Explainable AI',
 });
 
+// AIDEMOS_METADATA_FILE_NAME
+export const INDUSTRY = /** @type {const} */ ({
+  HEALTHCARE: 'Healthcare',
+  FINANCE: 'Finance',
+  MANUFACTURING: 'Manufacturing',
+  RETAIL: 'Retail',
+  TRANSPORTATION: 'Transportation',
+  EDUCATION: 'Education',
+  CUSTOMER_SERVICE: 'Customer Service',
+  ENTERTAINMENT: 'Entertainment',
+  AGRICULTURE: 'Agriculture',
+  CYBERSECURITY: 'Cybersecurity',
+});
+
+// AIDEMOS_METADATA_FILE_NAME
+export const TYPE = /** @type {const} */ ({
+    ROBOTICS: 'Robotics',
+    RECOMMENDATION_SYSTEMS: 'Recommendation Systems',
+    GENERATIVE_AI: 'Generative AI',
+    ANOMALY_DETECTION: 'Anomaly Detection',
+    SPEECH_RECOGNITION: 'Speech Recognition',
+    COMPUTER_VISION: 'Computer Vision',
+    NATURAL_LANGUAGE_PROCESSING: 'Natural Language Processing',
+})
+
+// AIDEMOS_METADATA_FILE_NAME
+export const USE_CASE_FUNCTIONALITY = /** @type {const} */ ({
+  REAL_TIME_VIDEO_PROCESSING: 'Real-time Video Processing',
+  PREDICTIVE_MAINTENANCE: 'Predictive Maintenance',
+  FRAUD_DETECTION: 'Fraud Detection',
+  SPEECH_TO_TEXT: 'Speech-to-Text',
+  RECOMMENDATION_ENGINE: 'Recommendation Engine',
+  CHATBOTS: 'Chatbots',
+  AUTONOMOUS_SYSTEMS: 'Autonomous Systems',
+  IMAGE_RECOGNITION: 'Image Recognition',
+  SENTIMENT_ANALYSIS: 'Sentiment Analysis',
+});
+
+
 export const TASKS = /** @type {const} */ ({
   MULTIMODAL: {
     TEXT_TO_IMAGE: 'Text-to-Image',
@@ -70,6 +109,39 @@ export const TASKS = /** @type {const} */ ({
 });
 
 export const TASKS_VALUES = Object.values(TASKS)
+  .map((v) => Object.values(v))
+  .flat();
+
+// AIDEMOS_METADATA_FILE_NAME
+export const TECHNOLOGY = /** @type {const} */ ({
+  AI_FRAMEWORKS: {
+    TENSORFLOW: 'TensorFlow',
+    PYTORCH: 'PyTorch',
+    OPENVINO: 'OpenVINO',
+    HUGGING_FACE: 'Hugging Face',
+    KERAS: 'Keras',
+    SCIKIT_LEARN: 'Scikit-learn',
+    AWS_SAGEMAKER: 'AWS SageMaker',
+    GOOGLE_CLOUD_AI: 'Google Cloud AI',
+  },
+  AI_TECHNIQUES: {
+    MACHINE_LEARNING: 'Machine Learning',
+    DEEP_LEARNING: 'Deep Learning',
+    COMPUTER_VISION: 'Computer Vision',
+    NATURAL_LANGUAGE_PROCESSING: 'Natural Language Processing',
+    REINFORCEMENT_LEARNING: 'Reinforcement Learning',
+    EDGE_COMPUTING: 'Edge Computing',
+    TRANSFORMERS: 'Transformers',
+    NEURAL_NETWORKS: 'Neural Networks',
+    },
+  COMPLEXITY_LEVEL : {
+    BEGINNER: 'Beginner',
+    INTERMEDIATE: 'Intermediate',
+    ADVANCED: 'Advanced',
+    EXPERT: 'Expert',
+  }
+})
+export const TECHNOLOGY_VALUES = Object.values(TECHNOLOGY)
   .map((v) => Object.values(v))
   .flat();
 
